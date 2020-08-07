74971c135539a569c5b0733204e81161daff3867
David Lam <david.lam@winc.com.au>
2020-08-07 11:36:06 +1000

    use exported variables instead of function

--- appname.go
+++ appname.go
@@ -5,12 +5,21 @@ import (
 	"path/filepath"
 )
 
-func Folder() (f string, n string) {
+var (
+	Folder         string
+	FolderAndSlash string
+	Name           string
+	NameWithExe    string
+	FilePath       string
+)
+
+func init() {
 	exe, _ := os.Executable()
-	f, n = filepath.Split(exe)
-	f = f[:len(f)-1] // get rid of trailing backslash of folder
-	if len(n) >= 4 && n[len(n)-4:] == ".exe" {
-		n = n[:len(n)-4] // get rid of .exe of name
+	FolderAndSlash, NameWithExe := filepath.Split(exe)
+	FilePath = FolderAndSlash + NameWithExe
+	Folder = FolderAndSlash[:len(FolderAndSlash)-1] // get rid of trailing backslash of folder
+	Name = NameWithExe
+	if len(Name) >= 4 && Name[len(Name)-4:] == ".exe" {
+		Name = Name[:len(Name)-4] // get rid of .exe of name
 	}
-	return
 }
